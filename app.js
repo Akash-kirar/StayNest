b2632d92630199b597aae3cb7b58f148d6c2ade7
Akash-kirar <akashkirar541@gmail.com>
2025-07-22 01:42:09 +0530

    working on database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+const path =require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,6 +18,12 @@ async function main() {
 }
 
 
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+
+
+
+
 
 
 app.get("/", (req, res) => {
@@ -24,19 +31,33 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/Testlisting", async (req, res) => {
-  let samleListing = new Listing({
-    title: "my new villa",
-    description: "by the beach",
-    price: 1200,
-    location: "goa",
-    country: "india",
+app.get("/listings", async (req, res) => {
+  const allListings= await Listing.find({});
+res.render("/listings/index.ejx", {allListings});
+    });
 
-  });
-  await samleListing.save();
-  console.log("sample was saved");
-  res.send("successful testing")
-});
+
+
+
+
+
+
+
+
+
+// app.get("/Testlisting", async (req, res) => {
+//   let samleListing = new Listing({
+//     title: "my new villa",
+//     description: "by the beach",
+//     price: 1200,
+//     location: "goa",
+//     country: "india",
+
+//   });
+//   await samleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing")
+// });
 
 app.listen (8080, () =>{
     console.log("server is listing to port 8080");
