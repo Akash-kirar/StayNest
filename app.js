9e6a4278c04e737e0577531f42ebb35cb658e186
Akash-kirar <akashkirar541@gmail.com>
2025-07-26 16:03:25 +0530

    next work

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
 const path =require("path");
+const { render } = require("ejs");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,29 +19,34 @@ async function main() {
 }
 
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-
-
-
-
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("hi , i am root");
 });
 
-
+//index route
 app.get("/listings", async (req, res) => {
   const allListings= await Listing.find({});
-res.render("/listings/index.ejx", {allListings});
+res.render("listings/index.ejs", {allListings});
     });
 
 
 
+    //new route top on the show route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
 
 
-
+    //show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing});
+});
 
 
 
