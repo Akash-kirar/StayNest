2c9790b9f14ca464c4b378dd74b5339e586bdf27
Akash-kirar <akashkirar541@gmail.com>
2025-07-28 13:59:31 +0530

    now user can update our listing

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
 const path =require("path");
+const methodOverride = require("method-override");
+
 const { render } = require("ejs");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -22,6 +24,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("hi , i am root");
@@ -59,7 +62,20 @@ res.redirect("/listings");
 });
 
 //edit route
+app.get("/listings/:id/edit", async (req, res) => {
+let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
+
+//update route
+app.put("/listings/:id", async (req, res) => {
+
+let { id } = req.params;
+await Listing.findByIdAndUpdate(id, {...req.body.listing});
+res.redirect(`/listings/${id}`);
+});
 
 
 
