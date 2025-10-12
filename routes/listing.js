18946e7d86df62758cab46758aaedf5632ff3b35
Akash-kirar <akashkirar541@gmail.com>
2025-10-13 01:46:11 +0530

    flash message complite

--- routes/listing.js
+++ routes/listing.js
@@ -68,6 +68,12 @@ res.redirect("/listings");
 router.get("/:id/edit", wrapAsync ( async (req, res) => {
 let {id} = req.params;
   const listing = await Listing.findById(id);
+
+ if(!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+
   res.render("listings/edit.ejs", { listing });
 }));
 
