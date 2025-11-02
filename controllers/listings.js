232128439e8edc887873f7285dcd3c9466a0a2b1
Akash-kirar <akashkirar541@gmail.com>
2025-11-02 22:59:44 +0530

    able to upload image complited

--- controllers/listings.js
+++ controllers/listings.js
@@ -62,15 +62,33 @@ let {id} = req.params;
     return res.redirect("/listings");
   }
 
-  res.render("listings/edit.ejs", { listing });
-}
+
+
+      let originalImageUrl = listing.image.url;
+
+      originalImageUrl = originalImageUrl.replace("/uploade" , "/uploade/w_250");
+
+
+
+
+
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
+};
 
 
 
   module.exports.updateListing = async (req, res) => {
 
 let { id } = req.params;
-await Listing.findByIdAndUpdate(id, {...req.body.listing});
+let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+
+if(typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+ listing.image = {url, filename};
+ await listing.save();
+}
+
 req.flash("success", "Listing Updated!");
 res.redirect(`/listings/${id}`);
 }
