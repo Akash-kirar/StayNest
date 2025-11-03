486012ca57147e96de43dffcfbab73a5eeaa5b57
Akash-kirar <akashkirar541@gmail.com>
2025-11-04 02:50:58 +0530

    working on map

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,7 @@
 const Listing = require("../models/listing");
-
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -25,26 +27,29 @@ module.exports.showListing=async (req, res) => {
      req.flash("error","Listing you requested for does not exist!!");
      res.redirect("/listings");
     }
-    console.log(listing);
+    // console.log(listing);
     res.render("listings/show.ejs", { listing });
  };
 
 
   module.exports.createListing = async (req, res, next) => {
+
+let response =  await geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1,
+})
+  .send();
+
+console.log(response.body.features[0].geometry);
+// res.send("Done!");
+
+
     let url = req.file.path;
     let filename = req.file.filename;
- 
-
-
 const newListing = new Listing(req.body.listing);   
 newListing.owner = req.user._id;
-
-
-    newListing.image = {url, filename};
-
-
+newListing.image = {url, filename};
 await newListing.save();
-
 req.flash("success", "New Listing Created!");
 res.redirect("/listings");
 
---
+++ public/js/map.js
@@ -0,0 +1,10 @@
+
+
+
+    mapboxgl.accessToken = mapToken ;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [ 78.182831,  26.22983], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    });
