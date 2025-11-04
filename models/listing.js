5c4f30c3fa8a01f58d6529fbd91ca3d9f7c834e3
Akash-kirar <akashkirar541@gmail.com>
2025-11-05 02:17:39 +0530

    working on map

--- models/listing.js
+++ models/listing.js
@@ -24,6 +24,22 @@ owner: {
   type: Schema.Types.ObjectId,
   ref: "User",
 },
+
+
+  geometry :{
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+
+
+
   });
  
 
